4fc9931044c26015e24b943865b16d84e89add7c
Yuriy Moskalev <y.moskalev@gmail.com>
2018-04-12 15:43:07 +0300

    add navigation Drawer

--- app/src/main/java/ru/techmas/magicmirror/activities/MainActivity.kt
+++ app/src/main/java/ru/techmas/magicmirror/activities/MainActivity.kt
@@ -2,16 +2,24 @@ package ru.techmas.magicmirror.activities
 
 
 import android.os.Bundle
+import android.support.design.widget.NavigationView
+import android.support.v7.app.ActionBarDrawerToggle
+import android.view.MenuItem
 
 import com.arellomobile.mvp.presenter.InjectPresenter
 import com.arellomobile.mvp.presenter.ProvidePresenter
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.app_bar_main.*
 
 import ru.techmas.magicmirror.R
 import ru.techmas.magicmirror.interfaces.views.MainView
 import ru.techmas.magicmirror.presenters.MainActivityPresenter
 import ru.techmas.magicmirror.utils.Injector
 
-class MainActivity : BaseActivity(), MainView {
+class MainActivity : BaseActivity(), MainView, NavigationView.OnNavigationItemSelectedListener {
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 
 
     companion object {
@@ -40,5 +48,15 @@ class MainActivity : BaseActivity(), MainView {
 
     }
 
+    private fun setUpDrawerMenu() {
+        setSupportActionBar(toolbar)
+        val toggle = ActionBarDrawerToggle(
+                this, activity_main, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        activity_main.addDrawerListener(toggle)
+        toggle.syncState()
+        nav_view.setNavigationItemSelectedListener(this)
+
+    }
 
 }
