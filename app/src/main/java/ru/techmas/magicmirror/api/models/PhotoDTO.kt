1876ff0e8bdacbeeaf492c78f962f259663d1292
natalia <n.antipieva@gmail.com>
2018-04-25 23:51:07 +0200

    вывод альбомов из API во фрагмент

--- app/src/main/java/ru/techmas/magicmirror/api/models/PhotoDTO.kt
+++ app/src/main/java/ru/techmas/magicmirror/api/models/PhotoDTO.kt
@@ -15,6 +15,8 @@ import com.google.gson.annotations.SerializedName
             "USER_RATE_ID": "47",
             "USER_FULL_DATA":
  */
+
+
 data class PhotoDTO(
         @SerializedName("ID")
         var id: Long? = null,
@@ -34,6 +36,9 @@ data class PhotoDTO(
         @SerializedName("PROPERTY_PHOTO_USER_VALUE")
         var photoUser: Long? = null,
 
+        @SerializedName("SORT")
+        var sort: Long? = null,
+
         @SerializedName("DETAIL_TEXT_TYPE")
         var textType: String? = null,
 
@@ -41,7 +46,11 @@ data class PhotoDTO(
         var userRate: String? = null,
 
         @SerializedName("USER_FULL_DATA")
-        var user: UserDTO? = null
+        var user: UserDTO? = null,
+
+        @SerializedName("USER_ID")
+        var userId: Long? = null
+
 
 //        @SerializedName("USER_GROUP")
 //        var userGroup: String? = null
--- app/src/main/java/ru/techmas/magicmirror/presenters/AlbumPresenter.kt
+++ app/src/main/java/ru/techmas/magicmirror/presenters/AlbumPresenter.kt
@@ -3,11 +3,37 @@ package ru.techmas.magicmirror.presenters
 import ru.techmas.magicmirror.interfaces.views.AlbumView
 
 import com.arellomobile.mvp.InjectViewState
+import ru.noixlab.drivepoint.adapters.BaseRecyclerAdapter
+import ru.techmas.magicmirror.api.RestApi
+import ru.techmas.magicmirror.api.models.ApiResponse
+import ru.techmas.magicmirror.models.Photos
+import ru.techmas.magicmirror.utils.RxUtils
+import ru.techmas.magicmirror.utils.presenter.PreferenceHelper
 
 import javax.inject.Inject
 
 
 @InjectViewState
 class AlbumPresenter @Inject
-internal constructor() : BasePresenter<AlbumView>()
+constructor(val restApi: RestApi, val preferenceHelper: PreferenceHelper) : BasePresenter<AlbumView>(), BaseRecyclerAdapter.OnItemClickListener {
+
+    override fun onClick(item: Any) {
+    }
+
+    init {
+        getAlbum()
+    }
+
+    private fun getAlbum() {
+        val request = restApi.album.getAlbum(preferenceHelper.token!!, null)
+                .compose(RxUtils.httpSchedulers())
+                .subscribe({ successGetList(it) }, { handleError(it) })
+        unSubscribeOnDestroy(request)
+    }
+
+    private fun successGetList(response: ApiResponse<Photos>) {
+        viewState.showData(response.data!!)
+    }
+
+}
 
